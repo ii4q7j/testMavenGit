09c1cb34a215fdc633c2fb76e5d40fd0d87f2fa1
ii4q7j <ii4q7j@gmail.com>
2017-01-25 23:55:19 +0300

    My first commit

---
+++ src/main/java/ru/test/maven/testMavenGit.java
@@ -0,0 +1,10 @@
+package ru.test.maven;
+
+/**
+ * Created by dmitriybala on 25.01.17.
+ */
+public class testMavenGit {
+    public static void main(String[] args) {
+        System.out.println("Hello, Github!");
+    }
+}
